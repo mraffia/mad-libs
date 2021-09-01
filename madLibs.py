c6e4136eed136230bd42d2b8394baf14b0e5c6df
Mohammad Raffi <mohraffia@gmail.com>
2021-09-02 01:48:01 +0700

    revise mad lib program and add example text file

--- madLibs.py
+++ madLibs.py
@@ -6,25 +6,29 @@ def madLibs(fileNameAndSuffix):
     content = oriFile.read()
     oriFile.close()
 
+    contentList = content.split(' ')
     exists = []
+    for i in range(len(contentList)):
+        if 'ADJECTIVE' in contentList[i]:
+            exists.append('ADJECTIVE')
+        elif 'NOUN' in contentList[i]:
+            exists.append('NOUN')
+        elif 'ADVERB' in contentList[i]:
+            exists.append('ADVERB')
+        elif 'VERB' in contentList[i]:
+            exists.append('VERB')
 
-    if 'ADJECTIVE' in content:
-        exists.append('ADJECTIVE')
-    if 'NOUN' in content:
-        exists.append('NOUN')
-    if 'ADVERB' in content:
-        exists.append('ADVERB')
-    if 'VERB' in content:
-        exists.append('VERB')
-
-    replacements = {}
-
+    replacements = []
     for i in range(len(exists)):
-        rep = input('Enter a/an ' + exists[i].lower() + ':\n')
-        replacements[exists[i]] = rep
+        if exists[i] == 'ADJECTIVE' or exists[i] == 'ADVERB':
+            rep = input('Enter an ' + exists[i].lower() + ':\n')
+            replacements.append(rep)
+        else:
+            rep = input('Enter a ' + exists[i].lower() + ':\n')
+            replacements.append(rep)
 
-    for word, rep in replacements.items():
-        content = content.replace(word, rep)
+    for i in range(len(replacements)):
+        content = content.replace(exists[i], replacements[i], 1)
 
     finalFile = open('result.txt', 'w')
     finalFile.write(content)
